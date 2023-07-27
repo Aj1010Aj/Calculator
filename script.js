942c367ad85199d4a9a214936ba3d5f897c7e9c6
Aj <ajaykvarghese@gmail.com>
2023-07-27 15:50:52 +0700

    Simple calculator using html,css,js

---
+++ script.js
@@ -0,0 +1,17 @@
+function buttonclick(val)
+{
+    console.log(val)
+    document.getElementById("screen").value+=val
+
+}
+function displayclear()
+{
+    document.getElementById("screen").value=""
+
+}
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
